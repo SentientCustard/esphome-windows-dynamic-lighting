00663ff631453d89c474a78ee9e15bc313970323
SentientCustard <142143380+SentientCustard@users.noreply.github.com>
2026-03-18 23:06:13 +0800

    Update usb_lamparray.cpp

--- components/usb_lamparray/usb_lamparray.cpp
+++ components/usb_lamparray/usb_lamparray.cpp
@@ -386,7 +386,10 @@ void USBLampArrayComponent::setup() {
 
   // String table (index 0 = language, 1 = manufacturer, 2 = product, 3 = serial)
   static char serial_buf[16];
-  snprintf(serial_buf, sizeof(serial_buf), "%08X", (unsigned)ESP.getEfuseMac());
+  
+  uint8_t mac[6];
+  esp_efuse_mac_get_default(mac);
+  snprintf(serial_buf, sizeof(serial_buf), "%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3]);
   s_string_descriptor[0] = (char[]){0x09, 0x04};  // English (US)
   s_string_descriptor[1] = this->manufacturer_;
   s_string_descriptor[2] = this->product_;
